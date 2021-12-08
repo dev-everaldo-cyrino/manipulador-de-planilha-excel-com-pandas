0c9445643a515bd79cc835999fa332cd43d1f399
dev-everaldo-cyrino <alonedev3d2a@gmail.com>
2021-12-08 13:48:31 -0300

    fim

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # manipulador-de-planilha-excel-com-pandas
  manipulação de dados de uma planilha excel com a biblioteca pandas
  
-caso você queira baixar e testar este programa, altere as variaveis para o nome da sua tabela excel, e coloque-a na mesma pasta que o arquivo main.py.
+caso você queira baixar e testar este programa, altere as variaveis para o nome da sua tabela excel que estão na linha 27-32, e coloque-a na mesma pasta que o arquivo main.py.
+os arquivos originais usados para a construção deste programa estão contidos na mesma pasta que ele.
+acesse: https://github.com/dev-everaldo-cyrino/manipulador-de-planilha-excel-com-pandas
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import pandas as pd
-gerentes = pd.read_excel("Gerentes.xlsx")
-vendas12 = pd.read_excel("Vendas -Dez.xlsx")
-vendas = pd.read_excel("Vendas.xlsx")
 while True:
-    op = input('''
+    op = int(input('''
     ----- menu -----
+    0 - escolha uma tabela
     1 - print dados
     2- pegando 1 linha
     3- pegando linhas correspondente a uma condição
-    4- pegando linhas correspondente a uma condição e colunas
+    4- Resumo dos dados
     5- criando uma nova coluna
     6- adicionando linhas
     7- excluindo linhas ou colunas
@@ -17,30 +15,129 @@ while True:
     10- calcular indicador simples
     11- calcular indicador composto
     12- mesclar DataFrame
-    0 - sair''')
-    if op ==1:
-        pass
+    13 - sair
+    :'''))
+    if op == 0:
+        op1 = int(input('''
+        -----escolha uma tabela-----
+        1- tabela gerentes
+        2- tabela vendas de dezembro
+        3- tabela total de vendas
+        :'''))
+        if op1 ==1:
+            tb = pd.read_excel("Gerentes.xlsx")
+        elif op1 ==2:
+            tb = pd.read_excel("Vendas - Dez.xlsx")
+        else:
+            tb = pd.read_excel("Vendas.xlsx")
+        print('----------------------------------------------')
+        
+    elif op ==1:
+        print(tb)
     elif op ==2:
-        pass
+        linha = int(input('qual linha deseja pegar: '))
+        try:
+            print(tb.loc[linha])
+        except:
+            print('linha não encontrada')
+        print('----------------------------------------------')     
     elif op ==3:
-        pass
+        coluna = input('nome da coluna:  ')
+        linha = input('nome da linha(ex:loja de roupas):  ')
+        try:
+            print(tb.loc[tb[coluna] == linha])
+        except:
+            print('dado não encontrada')
+        print('----------------------------------------------')
+            
     elif op ==4:
-        pass
+        print(tb.describe())
     elif op ==5:
-        pass
+        coluna = input('nome da coluna: ')
+        valor = float(input('dado da coluna nova: '))
+        try:
+            tb.loc[:,coluna] = valor
+            print('coluna criada com sucesso')
+        except:
+            print('erro ao criar a coluna')
+        print('----------------------------------------------')
+        
     elif op ==6:
-        pass
+        linha = input('nome do arquivo que sera anexado: ')
+        try:
+            linha = pd.read_excel(linha)
+            tb.append(linha)
+            print('dados anexados com sucesso')
+        except:
+            print('erro ao tentar anexar os dados')
+        print('----------------------------------------------')
+            
     elif op ==7:
-        pass
+        op = int(input('''
+        -----exluindo dados-----
+        0 - exluir linha
+        1 - exluir coluna'''))
+        dado = input('nome do dado excluido: ')
+        try:
+            tb = tb.drop(dado,axis=op)
+            print(tb)
+            print('dados excluidos com sucesso')
+        except:
+            print('erro ao tentar excluir os dados')
+        print('----------------------------------------------')
+            
     elif op ==8:
-        pass
+        dado = input('nome do dado: ')
+        try:
+            tb[dado] = tb[dado].fillna(tb[dado].mean())
+            print('dados preenchidos com sucesso')
+        except:
+            print('erro ao tentar preencher dados os dados')
+        print('----------------------------------------------')
+            
     elif op ==9:
-        pass
+        dado = input('nome do dado: ')
+        try:
+            tb.ffill()
+            print('dados preenchidos com sucesso')
+        except:
+            print('erro ao tentar preencher os dados')
+        print('----------------------------------------------')
+            
     elif op ==10:
-        pass
+        dado = input('nome da coluna: ')
+        try:
+            print(tb[dado].value_counts())
+        except:
+            print('erro ao tentar caulcular indicadores de dados')
+        print('----------------------------------------------')
+            
     elif op ==11:
-        pass
+        op1 = int(input('''
+        1- deseja ver a soma dos valores
+        2- deseja ver a media dos valores
+        :'''))
+        
+            
+        lista= input('nome da coluna1: ') 
+        lista2= input('nome da coluna2: ') 
+        try:
+            if op1 ==1:
+                print(tb[[lista,lista2]].groupby(lista).sum())
+            else:
+                print(tb[[lista,lista2]].groupby(lista).mean())
+        except:
+            print('erro ao tentar caulcular indicadores de dados')
+        print('----------------------------------------------')
+            
     elif op ==12:
-        pass
-    elif op ==0:
+        nome = input('nome da tabela: ')
+        try:
+            tb2 = pd.read_excel(nome)
+            tb = tb.merge(tb2)
+            print(tb)
+        except:
+            print('ocorreu algum erro')
+        print('----------------------------------------------')
+    elif op ==13:
         break
